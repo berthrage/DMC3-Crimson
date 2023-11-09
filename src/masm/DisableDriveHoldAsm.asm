bea5a810e11b2dab9f8e34ca7180be77fa3b32c5
SSSiyan <gsssiyan@gmail.com>
2023-11-09 13:06:39 +0000

    DisableDriveHold Dante compare

--- src/masm/DisableDriveHoldAsm.asm
+++ src/masm/DisableDriveHoldAsm.asm
@@ -3,11 +3,15 @@ extern g_DisableDriveHold_ReturnAddr:QWORD
 
 .CODE
 DisableDriveHoldDetour PROC ; player in RDI, rsi holds the offset for each charge
+    cmp dword ptr [rdi+78h], 0 ; dante id
+    jne CodeLabel
     push rdx
     lea rdx, [rdi+66a4h] ; rebellion charge offset
     cmp rdx, rsi ; is this rebellion's charge input?
     pop rdx
     je ReturnLabel
+
+CodeLabel:
     addss xmm0, dword ptr [rsi+08h]
 
 ReturnLabel:
