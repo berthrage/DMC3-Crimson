3aba44930580b4a2479f7e8b12e766cb6c9c9d40
Darkness <amir008hosein@gmail.com>
2024-08-22 20:05:07 +0330

    Here, read on: https://learn.microsoft.com/en-us/cpp/build/x64-calling-convention?view=msvc-170

--- src/masm/CreateEffect.asm
+++ src/masm/CreateEffect.asm
@@ -12,18 +12,22 @@ CreateEffectDetour PROC
 	mov     QWORD PTR [rsp+16], rdx ; effectBank
 	mov     QWORD PTR [rsp+8], rcx ; pPlayer
 	; rsp+0 is reserved for the return address
+	push 	rax
+	push	r10
+	push	r11
+	push 	rdi
 	xor 	r8,r8 ; not sure what this is used for
 	push 	rbx
 	mov 	rbx, [DetourBaseAddr]
 	mov 	rbx, [rbx + 0C18AF8h]
 	mov 	rbx, [rbx + 0C90E28h]
-	test 	rbx,rbx
+	test 	rbx, rbx
 	je 		return
 	mov 	rbx, [rbx + 18h] ; player
 	xor 	rcx, rcx
 	xor 	rdx, rdx
-	mov 	ecx, DWORD PTR [rsp+16+8] ; effectBank
-	mov 	edx, DWORD PTR [rsp+24+8] ; effectId
+	mov 	ecx, DWORD PTR [rsp+16+40] ; effectBank
+	mov 	edx, DWORD PTR [rsp+24+40] ; effectId
 	; where add colour
 	sub		rsp, 20h ; Shadow space for the call
 	call 	createEffectCallA ; from dmc3.exe+211B95, another example at dmc3.exe+20167F
@@ -36,19 +40,19 @@ CreateEffectDetour PROC
 	inc 	eax ; y pos
 
 	push 	r10 ; check player addr was given, temp until all calls are updated
-	mov 	r10, QWORD PTR [rsp+8+16] ; pPlayer
+	mov 	r10, QWORD PTR [rsp+8+48] ; pPlayer
 	test 	r10, r10
 	pop 	r10
 	je 		UseP1
 
-	mov 	rbx, QWORD PTR [rsp+8+8] ; pPlayer
+	mov 	rbx, QWORD PTR [rsp+8+40] ; pPlayer
 	xor 	rax, rax
 	cmp     byte ptr [rbx+3E9Bh], 00 ; devilTrigger
-	mov     eax, DWORD PTR [rsp+32+8] ; effectBoneIdx ; for skipped bone
+	mov     eax, DWORD PTR [rsp+32+40] ; effectBoneIdx ; for skipped bone
 	je      skipDTBoneAdd
 	mov     eax, 18h
 	imul    eax, dword ptr [rbx+3E88h] ; devilTriggerModel
-	add 	eax, DWORD PTR [rsp+32+8] ; effectBoneIdx
+	add 	eax, DWORD PTR [rsp+32+40] ; effectBoneIdx
 	skipDTBoneAdd:
 	mov 	rdx, [rbx + rax * 8h + 0000E5D0h]
 	jmp 	Cont
@@ -58,10 +62,14 @@ UseP1:
 	jmp 	Cont
 Cont:
 	mov 	rcx, [rdx + 00000110h]
-	mov 	[rdi + 000000C0h],rcx ; player xyz
+	mov 	[rdi + 000000C0h], rcx ; player xyz
 return:
 	pop 	rbx
 	;add rsp,40h handled by the callee in _fastcall
+	pop		rdi
+	pop		r11
+	pop 	r10
+	pop 	rax
 	ret
 CreateEffectDetour ENDP
 END
