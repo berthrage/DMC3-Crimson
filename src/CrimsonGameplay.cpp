d8aeac88052ee36dbebc5ff50c5ad1eb3a8375e0
Mia Berth <miaberthrage@gmail.com>
2024-09-22 08:02:47 -0300

    Testing more leniency to guardfly timer

--- src/CrimsonGameplay.cpp
+++ src/CrimsonGameplay.cpp
@@ -1454,7 +1454,7 @@ void StoreInertia(byte8* actorBaseAddr) {
 
     
 	// GUARDFLY TIMING
-    const float timing = 0.057f;
+    const float timing = 0.073f;
 	if (i->airGuard.cachedPull == 28.0f && event == ACTOR_EVENT::JUMP_CANCEL) {
 
 		if (guardflyTimer > timing) {
